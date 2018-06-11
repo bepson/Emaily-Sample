2022e0b5f0653e6f476e4560ca997c337d529671
Douglas Berg <berg.douglas@gmail.com>
2018-06-10 20:27:38 -0600

    Added Cookie-Session, initialized passport and connect to mlab mongodb database

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
+require('./models/User');
+require('./services/passport');
 const keys = require('./config/keys');
 
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
+require('./routes/authRoutes') (app);
 
-passport.use(new GoogleStrategy(
-    {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-    }, 
-    (accessToken, refreshToken, profile, done) => {
-        console.log('access token: ', accessToken);
-        console.log('refresh token: ', refreshToken);
-        console.log('profile: ', profile);
-    }));
-
-    app.get('/auth/google', passport.authenticate('google', {
-        scope: ['profile', 'email']
-    }));
-
-    app.get('/auth/google/callback', passport.authenticate('google'))
 const PORT = process.env.PORT;
 
 app.listen(PORT || 5000);
